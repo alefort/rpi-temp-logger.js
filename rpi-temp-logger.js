67e85f9aef6a0f60431442a7c5ca0eda6b94cdcf
Andre LeFort <andre@tbkcreative.com>
2014-01-02 15:45:54 -0500

    tweaking resistance calcs

--- rpi-temp-logger.js
+++ rpi-temp-logger.js
@@ -87,7 +87,7 @@ var spiADC = {
 	calculate_thermistor_resistance: function( average_raw ){
 		console.log('average raw: ' + average_raw);
 		console.log('series resistor: ' + this.series_resistor);
-		var thermistor_resistance =  (1023 / average_raw - 1);
+		var thermistor_resistance =  (1023 / (average_raw - 1) );
 		console.log('thermistor resistance: ' + thermistor_resistance);
 		thermistor_resistance = this.series_resistor / thermistor_resistance;
 		thermistor_resistance = Math.round(thermistor_resistance * 100) / 100;
