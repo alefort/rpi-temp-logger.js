d94239162b530c3ffdc4b9c72eccc8f3fe82b3b3
Andre LeFort <andre@tbkcreative.com>
2014-01-02 16:25:25 -0500

    tweaking resistance calcs

--- rpi-temp-logger.js
+++ rpi-temp-logger.js
@@ -20,7 +20,7 @@ var spiADC = {
 	'res_index': 0,	
 	'series_resistor': 10000,
 	'thermistor': {
-		'nominal_resistance': 97500,
+		'nominal_resistance': 10000,
 		//'nominal_resistance': 55500,
 		'nominal_temp': 25,
 		'bcoefficient': 3950
