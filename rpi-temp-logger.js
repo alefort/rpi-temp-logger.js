f9b3929eb6243db6d74076c8deeab26566c743a3
Andre LeFort <andre@tbkcreative.com>
2014-01-03 13:39:10 -0500

    trying to figure out my thermistors resistance

--- rpi-temp-logger.js
+++ rpi-temp-logger.js
@@ -18,10 +18,9 @@ var spiADC = {
 	'device': '',
 	'resistances': [],	
 	'res_index': 0,	
-	'series_resistor': 10000,
+	'series_resistor': 9860,
 	'thermistor': {
-		'nominal_resistance': 10000,
-		//'nominal_resistance': 55500,
+		'nominal_resistance': 24000,		
 		'nominal_temp': 25,
 		'bcoefficient': 3950
 	},	
